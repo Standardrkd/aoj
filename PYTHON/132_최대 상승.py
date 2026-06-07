cfa51f517e72155de4d93e0fb8e3974ba5e005e9
Standardrkd <pgj5927@gmail.com>
2026-06-07 23:29:03 +0900

    솔루션 생각

--- PYTHON/132_최대 상승.py
+++ PYTHON/132_최대 상승.py
@@ -1,14 +1,7 @@
 N = int(input())
 L = list(map(int, input().split()))
-results = []
-prefix = 0
-for i in range(N-1):
-    x = L[i+1] - L[i]
-    if x > 0:
-        prefix += x
-        results.append(prefix)
-    else:
-        prefix = 0
-print(results)
-print(max(results) if results else 0)
+
+d = dict() # 딕셔너리랑 정렬해서 비교하면서 뭐시기 하면 될듯? 
+
+
     
